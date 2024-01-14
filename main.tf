b2d7738f888f5b3589f17b893967bd95f019bd64
prasanth4a5 <ummadi1994@gmail.com>
2024-01-14 13:06:19 +0530

    elasticache

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://github.com/Prasanthreddy4a5/tf-module-vpc.git"
+  source = "git::https://github.com/prasanthreddy4a5/tf-module-vpc.git"
 
   for_each                   = var.vpc
   cidr                       = each.value["cidr"]
@@ -12,7 +12,7 @@ module "vpc" {
 }
 
 module "alb" {
-  source = "git::https://github.com/Prasanthreddy4a5/tf-module-alb.git"
+  source = "git::https://github.com/prasanthreddy4a5/tf-module-alb.git"
 
   for_each        = var.alb
   internal        = each.value["internal"]
@@ -26,7 +26,7 @@ module "alb" {
 }
 
 module "docdb" {
-  source = "git::https://github.com/Prasanthreddy4a5/tf-module-docdb.git"
+  source = "git::https://github.com/prasanthreddy4a5/tf-module-docdb.git"
   tags   = var.tags
   env    = var.env
 
@@ -44,7 +44,7 @@ module "docdb" {
 }
 
 module "rds" {
-  source = "git::https://github.com/Prasanthreddy4a5/tf-module-rds.git"
+  source = "git::https://github.com/prasanthreddy4a5/tf-module-rds.git"
   tags   = var.tags
   env    = var.env
 
@@ -66,7 +66,7 @@ module "rds" {
 }
 
 module "elasticache" {
-  source = "git::https://github.com/Prasanthreddy4a5/tf-module-elasticache.git"
+  source = "git::https://github.com/prasanthreddy4a5/tf-module-elasticache.git"
   tags   = var.tags
   env    = var.env
 
@@ -83,6 +83,55 @@ module "elasticache" {
   num_cache_nodes  = each.value["num_cache_nodes"]
   engine_version   = each.value["engine_version"]
 }
+
+module "rabbitmq" {
+  source  = "git::https://github.com/prasanthreddy4a5/tf-module-rabbitmq.git"
+  tags    = var.tags
+  env     = var.env
+  zone_id = var.zone_id
+
+  for_each = var.rabbitmq
+
+  subnet_ids       = local.db_subnets
+  vpc_id           = local.vpc_id
+  sg_ingress_cidr  = local.app_subnets_cidr
+  instance_type    = each.value["instance_type"]
+  ssh_ingress_cidr = var.ssh_ingress_cidr
+}
+
+module "app" {
+  depends_on = [module.docdb, module.alb, module.elasticache, module.rabbitmq, module.rds]
+  source     = "git::https://github.com/prasanthreddy4a5/tf-module-app.git"
+
+  tags             = var.tags
+  env              = var.env
+  zone_id          = var.zone_id
+  ssh_ingress_cidr = var.ssh_ingress_cidr
+  default_vpc_id   = var.default_vpc_id
+
+  for_each         = var.apps
+  component        = each.key
+  port             = each.value["port"]
+  instance_type    = each.value["instance_type"]
+  desired_capacity = each.value["desired_capacity"]
+  max_size         = each.value["max_size"]
+  min_size         = each.value["min_size"]
+  lb_priority      = each.value["lb_priority"]
+  parameters       = each.value["parameters"]
+
+  sg_ingress_cidr = local.app_subnets_cidr
+  vpc_id          = local.vpc_id
+  subnet_ids      = local.app_subnets
+
+  private_alb_name = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
+  public_alb_name = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
+  private_listener = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
+  public_listener = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
+}
+
+
+
+
 
 
 
