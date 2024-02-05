d9c8d7215db3646c32e46c75759327de088fcc55
Prasanthreddy4a5 <ummadi1994@gmail.com>
2024-02-05 20:13:59 +0530

    Test

--- vars.tf
+++ vars.tf
@@ -7,9 +7,9 @@ variable "tags" {}
 variable "env" {}
 variable "alb" {}
 variable "docdb" {}
-variable "rds" {}
-variable "elasticache" {}
-variable "rabbitmq" {}
-variable "zone_id" {}
-#variable "apps" {}
-variable "ssh_ingress_cidr" {}
+#variable "rds" {}
+#variable "elasticache" {}
+#variable "rabbitmq" {}
+#variable "zone_id" {}
+##variable "apps" {}
+#variable "ssh_ingress_cidr" {}
