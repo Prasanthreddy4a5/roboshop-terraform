3f7ecabccd9955f6fd03003bb6886abfcc02a650
Prasanthreddy4a5 <ummadiprashanthreddy@gmail.com>
2024-03-28 15:51:08 +0530

    Kubernetes

--- main.tf
+++ main.tf
@@ -160,6 +160,52 @@ module "rabbitmq" {
 
 
 
+#Elastic Kubernetes Service (Amazon EKS) module
+
+#Amazon EKS is a managed service that makes it easy for you to use Kubernetes on AWS without needing to install and operate your own Kubernetes control plane.
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "prod-roboshop"
+  cluster_version = "1.28"
+
+  cluster_endpoint_public_access  = false
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = local.vpc_id
+  subnet_ids               = local.app_subnets
+  control_plane_subnet_ids = local.app_subnets
+
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  tags = var.tags
+}
+
+
 
 
 
