cd965347e3ed857c5464eddc97e364645032fcef
Prasanthreddy4a5 <ummadiprashanthreddy@gmail.com>
2024-03-28 15:37:55 +0530

    Kubernetes

--- main.tf
+++ main.tf
@@ -14,22 +14,22 @@ module "vpc" {
 }
 
 
-
-module "alb" {
-  source = "git::https://github.com/prasanthreddy4a5/tf-module-alb.git"
-
-  for_each                   = var.alb
-  tags                       = var.tags
-  env                        = var.env
-  internal                   = each.value["internal"]
-  lb_type                    = each.value["lb_type"]
-  sg_ingress_cidr            = each.value["sg_ingress_cidr"]
-  vpc_id                     = each.value["internal"] ? local.vpc_id : var.default_vpc_id
-  subnets                    = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.ids
-  sg_port                    = each.value["sg_port"]
-  acm_certifiate_arn         = var.acm_certificate_arn
-
-}
+#
+#module "alb" {
+#  source = "git::https://github.com/prasanthreddy4a5/tf-module-alb.git"
+#
+#  for_each                   = var.alb
+#  tags                       = var.tags
+#  env                        = var.env
+#  internal                   = each.value["internal"]
+#  lb_type                    = each.value["lb_type"]
+#  sg_ingress_cidr            = each.value["sg_ingress_cidr"]
+#  vpc_id                     = each.value["internal"] ? local.vpc_id : var.default_vpc_id
+#  subnets                    = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.ids
+#  sg_port                    = each.value["sg_port"]
+#  acm_certifiate_arn         = var.acm_certificate_arn
+#
+#}
 
 module "docdb" {
   source = "git::https://github.com/prasanthreddy4a5/tf-module-docdb.git"
@@ -114,42 +114,54 @@ module "rabbitmq" {
 
 
 
+#
+#module "app" {
+#  depends_on = [module.docdb, module.alb, module.elasticache, module.rabbitmq, module.rds]
+#  source     = "git::https://github.com/prasanthreddy4a5/tf-module-app.git"
+#
+#  tags                    = merge(var.tags, each.value["tags"])
+#  env                     = var.env
+#  zone_id                 = var.zone_id
+#  ssh_ingress_cidr        = var.ssh_ingress_cidr
+#  default_vpc_id          = var.default_vpc_id
+#  monitoring_ingress_cidr = var.monitoring_ingress_cidr
+#  az                      = var.az
+#  kms_key_id              = var.kms_key_id
+#
+#  for_each         = var.apps
+#  component        = each.key
+#  port             = each.value["port"]
+#  instance_type    = each.value["instance_type"]
+#  desired_capacity = each.value["desired_capacity"]
+#  max_size         = each.value["max_size"]
+#  min_size         = each.value["min_size"]
+#  lb_priority      = each.value["lb_priority"]
+#  parameters       = each.value["parameters"]
+#
+#  sg_ingress_cidr = local.app_subnets_cidr
+#  vpc_id          = local.vpc_id
+#  subnet_ids      = local.app_subnets
+#
+#  private_alb_name = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
+#  public_alb_name = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
+#  private_listener = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
+#  public_listener = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
+#
 
-module "app" {
-  depends_on = [module.docdb, module.alb, module.elasticache, module.rabbitmq, module.rds]
-  source     = "git::https://github.com/prasanthreddy4a5/tf-module-app.git"
 
-  tags                    = merge(var.tags, each.value["tags"])
-  env                     = var.env
-  zone_id                 = var.zone_id
-  ssh_ingress_cidr        = var.ssh_ingress_cidr
-  default_vpc_id          = var.default_vpc_id
-  monitoring_ingress_cidr = var.monitoring_ingress_cidr
-  az                      = var.az
-  kms_key_id              = var.kms_key_id
-
-  for_each         = var.apps
-  component        = each.key
-  port             = each.value["port"]
-  instance_type    = each.value["instance_type"]
-  desired_capacity = each.value["desired_capacity"]
-  max_size         = each.value["max_size"]
-  min_size         = each.value["min_size"]
-  lb_priority      = each.value["lb_priority"]
-  parameters       = each.value["parameters"]
-
-  sg_ingress_cidr = local.app_subnets_cidr
-  vpc_id          = local.vpc_id
-  subnet_ids      = local.app_subnets
-
-  private_alb_name = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
-  public_alb_name = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
-  private_listener = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
-  public_listener = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
+#resource "aws_instance" "load_runner" {
+#  ami                    = data.aws_ami.ami.id
+#  vpc_security_group_ids = ["sg-0dee954b08055e577"]
+#  instance_type          = "t3.medium"
+#  tags = {
+#    Name = "Load-Runner"
+#  }
+#}
 
 
 
-}
+
+
 
 
 
